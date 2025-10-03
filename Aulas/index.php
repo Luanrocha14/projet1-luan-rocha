48bddf52fb3909a889a151597e20b5a7dfb03acb
Luan Rocha <luanrochasantiago@gmail.com>
2025-10-03 13:57:33 -0300

    peimeira mudança

---
+++ Aulas/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Testando scripts PHP</title>
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+  <body>
+    <header>
+      <h1>Apresente para nós</h1>
+    </header>
+    <section>
+      <form action="cad.php" method="get">
+        <label for="nome">Nome</label>
+        <input type="text" name="nome" id="idnome">
+
+        <label for="sobrenome">sobrenome</label>
+        <input type="text" name="sobrenome" id="idsobrenome">
+
+        <input type="submit" valoe="Enviar">
+      </form>
+    </section>
+  </body>
+
+</html>
